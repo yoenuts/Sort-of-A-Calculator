59df85e7a1c80f08d27258bcddd933f58f02a70c
Erlein Nicole <111441283+yoenuts@users.noreply.github.com>
2023-08-31 11:57:50 +0800

    Update calc.inc.php
    change file path, echo result

--- includes/calc.inc.php
+++ includes/calc.inc.php
@@ -1,15 +1,15 @@
 <?php 
 declare(strict_types = 1);
-include_once 'autoloader.inc.php';
+include_once '../classes/calc.class.php';
 
 $operator = $_POST["oper"];
 $num1 = $_POST["num1"];
 $num2 = $_POST["num2"];
 
-$calc = new Calc\Calc($operator, (int)$num1,(int)$num2);
+$calc = new \Calc\Calc($operator, (int)$num1,(int)$num2);
 
 try{
-    $calc -> Calculator();
+    echo $calc -> Calculator();
 } catch(TypeError $e){
     echo "Error!" . $e ->getMessage(); 
 }
